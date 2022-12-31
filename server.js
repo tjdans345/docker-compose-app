e5cf8c588c5384e8aa59c81471dd06d2916bee1a
meteor <tjdans34568@gmail.com>
2022-12-31 15:28:12 +0900

    server js create

--- server.js
+++ server.js
@@ -8,6 +8,18 @@ const client = redis.createClient({
 });
 
 const app = express();
+
+// 숫자는 0 부터 시작합니다.
+client.set("number", 0);
+
+app.get("/", (req, res) => {
+  client("number", (err, number) => {
+    // 현재 숫자를 가져온 후에 1씩 올려줍니다.
+    client.set("number", parseInt(number + 1));
+    res.send(`숫자가 1씩 올라갑니다 ${number}`);
+  });
+});
+
 app.listen(9060);
 
 console.log("server start !!!!!!!!!!!");
