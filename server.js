ea5612f1512bef724745df6684eb4ac72c605f37
meteor <tjdans34568@gmail.com>
2022-12-31 15:19:30 +0900

    first init

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const redis = require("redis");
+
+// 레디스 클라이언트 생성
+const client = redis.createClient({
+  host: "",
+  port: 6379,
+});
+
+const app = express();
+app.listen(9060);
+
+console.log("server start !!!");
